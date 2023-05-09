9493c52d03fb58d630b50b8780b14b841c198f94
Lakshit sharma <lakshlakshit18@gmail.com>
2023-05-09 21:54:52 +0530

    log_in screen updated

--- lib/pages/signin_page.dart
+++ lib/pages/signin_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
+import 'package:mentalhealthapp/pages/signup_page.dart';
 import 'package:mentalhealthapp/util/colors_utils.dart';
+
+import '../reusable_widgets/reusable_widgets.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -11,6 +14,8 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,11 +30,26 @@ class _SignInScreenState extends State<SignInScreen> {
         child: SingleChildScrollView(
             child: Padding(
           padding: EdgeInsets.fromLTRB(
-              20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+              20, MediaQuery.of(context).size.height * 0.2, 10, 0),
           // ignore: prefer_const_literals_to_create_immutables
           child: Column(
             children: <Widget>[
-              logoWidget("assets/images/logo2.jpeg"),
+              logoWidget("assets/images/pngegg.png"),
+              SizedBox(
+                height: 10,
+              ),
+              reusableTextFeild("Enter UserName", Icons.person_outline, false,
+                  _emailTextController),
+              SizedBox(
+                height: 10,
+              ),
+              reusableTextFeild("Enter Password", Icons.lock_outline, true,
+                  _passwordTextController),
+              SizedBox(
+                height: 10,
+              ),
+              signInSignUpButton(context, true, () {}),
+              signUpOption()
             ],
           ),
         )),
@@ -37,13 +57,27 @@ class _SignInScreenState extends State<SignInScreen> {
     );
   }
 
-  Image logoWidget(String imageName) {
-    return Image.asset(
-      imageName,
-      fit: BoxFit.cover,
-      width: 240,
-      height: 240,
-      color: Colors.white,
+  Row signUpOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          "Don't have account?",
+          style: TextStyle(color: Colors.white70),
+        ),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context,
+                // ignore: prefer_const_constructors
+                MaterialPageRoute(builder: (context) => SignUpScreen()));
+          },
+          child: const Text(
+            "Sign Up",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
     );
   }
 }
